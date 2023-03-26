db145f0ae626bb9b51a3d236bb17d08f09e0e525
Alexandra <102197498+AlexandraRodCas@users.noreply.github.com>
2023-03-25 20:52:07 -0700

    Initial commit

---
+++ app/src/main/java/alexandra/rodriguez/seguimientocuidadomascotas/ContrasenaActivity.kt
@@ -0,0 +1,11 @@
+package alexandra.rodriguez.seguimientocuidadomascotas
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ContrasenaActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_contrasena)
+    }
+}
---
+++ app/src/main/java/alexandra/rodriguez/seguimientocuidadomascotas/MainActivity.kt
@@ -0,0 +1,33 @@
+package alexandra.rodriguez.seguimientocuidadomascotas
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        actionBar?.hide()
+        val btn_registrarse: TextView = findViewById(R.id.tv_crearCuenta)
+        val btn_contra: TextView = findViewById(R.id.tv_olvidasteContra)
+        val btn_ingresar: Button = findViewById(R.id.btn_ingresar)
+
+        btn_registrarse.setOnClickListener{
+            val intentR: Intent = Intent(this, CrearcuentaActivity::class.java)
+            startActivity(intentR)
+        }
+
+        btn_contra.setOnClickListener{
+            val intentC: Intent = Intent(this, ContrasenaActivity::class.java)
+            startActivity(intentC)
+        }
+
+        btn_ingresar.setOnClickListener{
+            val intentI: Intent = Intent(this, DuenoperfilActivity::class.java)
+            startActivity(intentI)
+        }
+    }
+}
