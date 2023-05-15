0443ec36c35d04fa4d6fd683be298095f57f9967
Alexandra <102197498+AlexandraRodCas@users.noreply.github.com>
2023-05-15 15:44:12 -0700

    correccion que no subí

--- app/src/main/java/alexandra/rodriguez/seguimientocuidadomascotas/pesito/PesodActivity.kt
+++ app/src/main/java/alexandra/rodriguez/seguimientocuidadomascotas/pesito/PesodActivity.kt
@@ -15,6 +15,7 @@ import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.animation.TranslateAnimation
 import android.widget.*
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AlertDialog
@@ -26,7 +27,9 @@ import java.text.DecimalFormat
 import java.text.SimpleDateFormat
 import java.time.LocalDate
 import java.time.LocalTime
+import java.time.format.DateTimeFormatter
 import java.util.*
+import java.util.regex.Pattern
 
 class PesodActivity : AppCompatActivity() {
     private lateinit var storage: FirebaseFirestore
@@ -82,7 +85,7 @@ class PesodActivity : AppCompatActivity() {
             var imagenS: String = bundle.getString("uri").toString()
             val imagenUri = Uri.parse(imagenS)
             mascota = Mascota(bundle.getString("nombre").toString(), bundle.getInt("image"), imagenUri, bundle.getString("edad").toString() )        }
-
+        cargarDatos()
         btn_back.setOnClickListener {
             var intento = Intent(this, SignosvActivity::class.java)
             intento.putExtra("nombre",  mascota.nombre)
@@ -99,25 +102,143 @@ class PesodActivity : AppCompatActivity() {
 
     }
 
+    @RequiresApi(Build.VERSION_CODES.O)
     fun cargarDatos(){
-        val currentDate = SimpleDateFormat("d 'de' MMMM 'de' yyyy", Locale.getDefault()).format(Date())
         datoManual = ArrayList()
+        val pattern = Pattern.compile("\\d+\\.\\d+")
+        var peso: ArrayList<Double> = ArrayList()
+        var pesoT: ArrayList<Double> = ArrayList()
+        var pesoActualT = ""
         storage.collection("Peso")
             .whereEqualTo("email", correo)
             .whereEqualTo("mascota", mascota.nombre)
-            .whereEqualTo("fecha", currentDate)
             .get()
-            .addOnSuccessListener {
-                    documents ->
-                if(documents.size()>0) {
-                    val pesoActual:TextView = findViewById(R.id.pesoActual)
-                    for (document in documents){
-                        pesoActual.text = document.getString("kg")
+            .addOnSuccessListener { documents ->
+                if (documents.size() > 0) {
+                    var fechas: ArrayList<String> = ArrayList()
+                    val pesoActual: TextView = findViewById(R.id.pesoActual)
+                    for (document in documents) {
+                        val fechaActual: String = document.getString("fecha").toString()
+                        if (fechas.size > 0) {
+                            if (fechaExiste(fechas, fechaActual)) {
+                                continue
+                            } else {
+                                fechas.add(fechaActual)
+                            }
+                        } else {
+                            fechas.add(fechaActual)
+                        }
                     }
+                    if (fechas.isNotEmpty()) {
+                        // Ordenar la lista de fechas utilizando el comparador personalizado
+                        val fechasOrdenadas = fechas.sortedWith(Comparator { fecha1, fecha2 ->
+                            compararFechas(fecha1, fecha2)
+                        })
+                        val fechasOrdenadasSeparadas = ordenarFechas(fechasOrdenadas)
+
+                        for (fecha in fechasOrdenadasSeparadas) {
+                            for (document in documents) {
+                                if(fechasOrdenadasSeparadas.first().equals(document.getString("fecha"))){
+                                    pesoActualT = document.getString("kg").toString()
+                                    pesoActual.text = pesoActualT
+                                }
+                                if (fecha == document.getString("fecha")) {
+                                    val pesoD = document.getString("kg").toString()
+                                    if (coincideMesYFechaActual(fecha)) {
+                                        val matcher = pattern.matcher(pesoD)
+                                        if (matcher.find()) {
+                                            val pesoDouble = matcher.group().toDouble()
+                                            peso.add(pesoDouble)
+                                        }
+                                    }
+
+                                    val matcherT = pattern.matcher(pesoD)
+                                    if (matcherT.find()) {
+                                        val pesoDoubleT = matcherT.group().toDouble()
+                                        pesoT.add(pesoDoubleT)
+                                    }
+                                }
+                            }
+                        }
+
+                        val progreso: TextView = findViewById(R.id.progreso) as TextView
+                        val et_meta: TextView = findViewById(R.id.meta) as TextView
+                        val cambioMes: TextView = findViewById(R.id.cambioMes) as TextView
+                        var pesoMes = 0.0
+                        var pesoTotal = 0.0
+
+                        val str = et_meta.text
+                        val target = "kg"
+                        val target2 = "Kg"
+
+                        // Obtén el subconjunto del string antes del target
+                        val subStr = str.substring(0, str.indexOf(target))
+                        // Obtén el subconjunto del string antes del target
+                        Log.d("pesoActualT", pesoActualT)
+                        //var subStr2 = ""
+                        //if(pesoActualT.length > 0){
+                        //    subStr2 = pesoActualT.substring(0, pesoActualT.indexOf(target2))
+                        //}
+
+                            if (pesoT.isNotEmpty()) {
+                                if(peso.isNotEmpty()){
+                                    pesoMes = peso.last() - peso.last()
+                                }
+                                pesoTotal = pesoT.first() - pesoT.last()
+                                if(pesoMes < 0){
+                                    pesoMes = pesoMes*-1
+                                    cambioMes.text = "Perdió $pesoMes kg"
+                                }else {
+                                    cambioMes.text = "Ganó $pesoMes kg"
+                                }
+                                if(pesoTotal < 0){
+                                    pesoTotal = pesoTotal*-1
+                                    progreso.text = "Perdió $pesoTotal kg"
+                                } else{
+                                    progreso.text = "Ganó $pesoTotal kg"
+                                }
+                            }
+                        val imageView: ImageView = findViewById(R.id.circle)
+
+                        // Obtén la posición actual del ImageView
+                        val currentX = imageView.x
+                        val currentY = imageView.y
+                        var newX = currentX
+                        // Calcula la nueva posición a la que quieres mover el ImageView
+                        //if(subStr2.toInt() < subStr.toInt()){
+                        //    newX = currentX// Desplazamiento en el eje X
+                        //}
+                        newX = currentX + 2000
+
+                        // Crea una animación de desplazamiento desde la posición actual a la nueva posición
+                        val animation = TranslateAnimation(0f, newX - currentX, 0f, currentY)
+                        animation.duration = 1000 // Duración de la animación en milisegundos
+
+                        // Aplica la animación al ImageView y actualiza su posición final
+                        imageView.startAnimation(animation)
+                        imageView.x = newX
+                        imageView.y = currentY
+
+                    }
+
+                    }else{
+                    Toast.makeText(this, "no se ha ingresado datos", Toast.LENGTH_SHORT).show()
                 }
+
             }.addOnFailureListener{
                 Toast.makeText(this, it.toString(), Toast.LENGTH_SHORT).show()
             }
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    fun coincideMesYFechaActual(fechaString: String): Boolean {
+        val formatoFecha = DateTimeFormatter.ofPattern("d 'de' MMMM 'de' yyyy", Locale.getDefault())
+        val fecha = LocalDate.parse(fechaString, formatoFecha)
+        val mesFecha = fecha.monthValue
+        val añoFecha = fecha.year
+        val mesActual = LocalDate.now().monthValue
+        val añoActual = LocalDate.now().year
+        return mesFecha == mesActual && añoFecha == añoActual
     }
 
     @RequiresApi(Build.VERSION_CODES.O)
@@ -138,6 +259,21 @@ class PesodActivity : AppCompatActivity() {
                 }
 
             }
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    fun compararFechas(fecha1: String, fecha2: String): Int {
+        val format = SimpleDateFormat("dd 'de' MMMM 'de' yyyy", Locale.getDefault())
+        val date1 = format.parse(fecha1)
+        val date2 = format.parse(fecha2)
+
+        return date1.compareTo(date2)
+
+    }
+
+    fun ordenarFechas(fechas: List<String>): List<String> {
+        val format = SimpleDateFormat("dd 'de' MMMM 'de' yyyy", Locale.getDefault())
+        return fechas.sortedByDescending { format.parse(it) }
     }
 
     @RequiresApi(Build.VERSION_CODES.O)
@@ -217,7 +353,7 @@ class PesodActivity : AppCompatActivity() {
                         .add(actividad)
                         .addOnSuccessListener {
                             Toast.makeText(this, "Peso agregada", Toast.LENGTH_SHORT).show()
-                            //cargarDatos()
+                            cargarDatos()
                             dialog.dismiss()
                         }.addOnFailureListener {
                             Toast.makeText(
@@ -249,4 +385,6 @@ class PesodActivity : AppCompatActivity() {
         val dateObj = sdf.parse(hora)
         return SimpleDateFormat("hh:mm a", Locale.getDefault()).format(dateObj)
     }
+
+
 }
